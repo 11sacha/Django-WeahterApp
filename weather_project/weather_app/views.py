3541339e6c7f3c78a81b99c9e429e7d9d2b6b288
sacha11 <s.j.g_97@hotmail.com>
2024-09-26 09:58:55 -0300

    fetch weather and forecast data func in dev

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -13,3 +13,24 @@ def index(request):
         city2 = request.get('city2', None)
     else:
         return render(request, "weather_app/index.html")
+
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+
+
+    weather_data = {
+        'city': city,
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+    daily_forecasts = []
+    for daily_data in forecast_response['daily'][:5]:
+        daily_forecasts.append({
+            'day': datetime.datetime.fromtimestamp(daily_data['dt'].strftime('%A')),
+            'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+            'max_temp': round(daily_data['temp']['max'] - 273.15, 2)
+        })
